4add1b8f0de73a09411e68296811175bfac4d06f
Yichong Ma <mayichong123@gmail.com>
2018-09-25 21:27:43 -0400

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,15 +1,15 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('https://damp-ravine-94465.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('http://localhost:8080/api/listings', listing);
+	  return $http.post('https://damp-ravine-94465.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
-		return $http.delete('http://localhost:8080/api/listings/' + id);
+		return $http.delete('https://damp-ravine-94465.herokuapp.com/api/listings/' + id);
 	   /**TODO
         return result of HTTP delete method
        */
